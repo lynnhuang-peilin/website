dca0670bb42053b8afb0661ddec251c26b905ae6
lynnhuang-peilin <lynnhuang.peilin@gmail.com>
2025-08-29 22:22:47 -0400

    setup

---
+++ docs/Render all.R
@@ -0,0 +1,7 @@
+rmarkdown::render("index.Rmd", output_format = "html_document")
+rmarkdown::render("ISPOR.Rmd", output_format = "html_document")
+rmarkdown::render("team.Rmd", output_format = "html_document")
+rmarkdown::render("table.Rmd", output_format = "html_document")
+rmarkdown::render("gallery.Rmd", output_format = "html_document")
+rmarkdown::render("follow.Rmd", output_format = "html_document")
+
